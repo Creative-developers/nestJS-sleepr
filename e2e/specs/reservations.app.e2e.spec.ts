7d804bc13e275b20f53f05d3e6139e8afdc4aaf7
Ahmed Nasrullah <ahmed@adwora.com>
2024-12-22 15:57:38 +0400

    updated code for e2e

--- e2e/specs/reservations.app.e2e.spec.ts
+++ e2e/specs/reservations.app.e2e.spec.ts
@@ -20,38 +20,52 @@ describe('reservations', () => {
     });
 
     jwt = await response.text();
-    console.log(jwt);
   });
 
-  test('Create', async () => {
-    expect(true).toBeTruthy();
-    // console.log({ jwt });
-    // const response = await fetch('http://reservations:3000/reservations', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authentication: jwt,
-    //   },
-    //   body: JSON.stringify({
-    //     startDate: '01/12/2024',
-    //     endDate: '01/12/2024',
-    //     extra: 2,
-    //     placeId: '12345',
-    //     invoiceId: '493',
-    //     charge: {
-    //       amount: 5,
-    //       card: {
-    //         cvc: '123',
-    //         exp_month: 12,
-    //         exp_year: 2027,
-    //         number: '4242424242424242',
-    //       },
-    //     },
-    //   }),
-    // });
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
 
-    // expect(response.ok).toBeTruthy();
-    // const reservation = await response.json();
-    // console.log({ reservation });
+    const response = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+      },
+    );
+
+    const reservation = await response.json();
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const response = await fetch('http://reservations:3000/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authentication: jwt,
+      },
+      body: JSON.stringify({
+        startDate: '01/12/2024',
+        endDate: '01/12/2024',
+        extra: 2,
+        placeId: '12345',
+        invoiceId: '493',
+        charge: {
+          amount: 5,
+          card: {
+            cvc: '123',
+            exp_month: 12,
+            exp_year: 2027,
+            number: '4242424242424242',
+          },
+        },
+      }),
+    });
+
+    expect(response.ok).toBeTruthy();
+
+    return response.json();
+  };
 });
